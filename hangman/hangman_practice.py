6f14a1035d344ae6b4aa5ff02e937b0b31146d0b
oliviazura <oszura@student.ysu.edu>
2026-04-14 16:32:11 -0400

    Added personal notes for hangman practice

--- hangman/hangman_practice.py
+++ hangman/hangman_practice.py
@@ -10,4 +10,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 env = gym.make('ALE/Hangman-v5', render_mode = "rgb_array")
-play(env)
+#play(env)
+
+'''
+    from this observe that only actions are up (2), down (5), and fire (1) to navigate letters and entering them. 
+    discarded letters are removed from the list, 
+    agent should: guess a letter
+    
+    11 attempts before failure
+
+    11 most common letters: e, t, a, o, i , n, s, h, r, d, l
+    note that brute forcing could work in a lot of cases, but for a word like "ugly" would not work
+    
+    ideas
+    a: force model to iterate through these letters first
+    b: force model to iterate vowels first
+    c: observe if model converges to guessing these letters first 
+
+'''
