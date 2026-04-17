55e85a234e50cd23126399d85097a055422d2c30
oliviazura <oszura@student.ysu.edu>
2026-04-16 22:54:41 -0400

    added constants file and implemented changes

---
+++ project/constants.py
@@ -0,0 +1,9 @@
+MAX_NUM_EPISODES = 500
+STEPS_PER_EPISODE = 200
+
+EPSILON_MIN = 0.005
+max_num_steps = MAX_NUM_EPISODES * STEPS_PER_EPISODE
+EPSILON_DECAY = 500 * EPSILON_MIN / max_num_steps
+ALPHA = 0.05  # Learning rate
+GAMMA = 0.98  # Discount factor
+NUM_DISCRETE_BINS = 30  # Number of bins to Discretize each observation dim
