984df31e9a568bbffbeb703ebe3857367cb07a5f
oliviazura <oszura@student.ysu.edu>
2026-04-17 21:39:47 -0400

    added video to main

--- project/main.py
+++ project/main.py
@@ -1,13 +1,18 @@
 import gymnasium as gym
+import constants as const
+from gymnasium.wrappers import RecordVideo
 from q_learner import Q_Learner
 from util import train, test
 
-env = gym.make('MountainCar-v0')
+env = gym.make('MountainCar-v0', render_mode = "rgb_array")
 agent = Q_Learner(env)
+env = RecordVideo(
+    env,
+    video_folder = "project",
+    name_prefix = "mountaincar",
+    episode_trigger = lambda x: x % const.TRAINING_PERIOD == 0
+)
 learned_policy = train(agent, env)
-# Use the Gym Monitor wrapper to evalaute the agent and record video
-gym_monitor_path = "./gym_monitor_output"
-env = gym.wrappers.Monitor(env, gym_monitor_path, force=True)
 for _ in range(1000):
     test(agent, env, learned_policy)
 env.close()
